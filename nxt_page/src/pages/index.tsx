65b8bc02fb2f0be33f9aeafa3724e73c19b6e2c6
Luciano Lee <luciano.lee@webcomm.com.tw>
2023-09-04 16:53:25 +0800

    調整 page info整合功能

--- nxt_page/src/pages/index.tsx
+++ nxt_page/src/pages/index.tsx
@@ -30,35 +30,47 @@ interface RickandmortyCharacterRes {
   results: RickandmortyCharacter[]
 }
 
+interface PageInfo {
+  pageUrl: string;
+  next: string | null,
+  prev: string | null,
+  curr: number;
+}
+
 export default function Home() {
   // 基本上都和你寫react沒有區別
-  const [count, setCount] = useState<number>(0);
-  const [dataUrl, setDataurl] = useState<string>('https://rickandmortyapi.com/api/character');
+  // const [count, setCount] = useState<number>(0);
+  const [pageInfo, setPageInfo] = useState<PageInfo>({
+    pageUrl:'https://rickandmortyapi.com/api/character',
+    next: null,
+    prev: null,
+    curr: 0
+  });
   const [resData, setResData] = useState<RickandmortyCharacterRes|null>(null)
   const pageChange = (status: string) => {
     if (resData) {
       if (status === 'next') {
-        setDataurl(resData.info.next as string);
-        setCount(pre => pre += 1)
+        setPageInfo(pre => ({...pre, pageUrl: pre?.next!, curr: pre.curr + 1 }));
       }
       if (status === 'prev') {
-        setDataurl(resData.info.prev as string);
-        setCount(pre => pre -= 1)
+        setPageInfo(pre => ({...pre, pageUrl: pre?.prev!, curr: pre.curr - 1 }));
       }
     }
   }
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-    fetch(dataUrl, {
+    fetch(pageInfo.pageUrl, {
       signal: signal
     })
     .then((response) => response.json())
-    .then((response) => {
+    .then((response: RickandmortyCharacterRes) => {
+      const info = response.info
       setResData(response)
+      setPageInfo(pre => ({...pre, next: info.next, prev: info.prev}))
     });
     return () => controller.abort();
-  }, [dataUrl, resData])
+  }, [pageInfo.pageUrl, resData])
   return (
     <main className="container mx-auto">
       <h2>這裡會是首頁主要內容</h2>
@@ -77,12 +89,12 @@ export default function Home() {
         <button 
           className="p-2 mx-2 rounded-lg shadow-md bg-blue-500 hover:bg-blue-300 disabled:bg-gray-100 min-w-40"
           onClick={() => pageChange('prev')}
-          disabled={resData?.info.prev===null} >-</button>
-        <div>{count}</div>
+          disabled={pageInfo.prev===null} >-</button>
+        <div>{pageInfo.curr}</div>
         <button 
           className="p-2 mx-2 rounded-lg shadow-md bg-blue-500 hover:bg-blue-300 min-w-40"
           onClick={() => pageChange('next')}
-          disabled={resData?.info.next===null}>+</button>
+          disabled={pageInfo.next===null}>+</button>
       </div>
       {resData?.results?.map((character) => (
         <div key={character.id}>
