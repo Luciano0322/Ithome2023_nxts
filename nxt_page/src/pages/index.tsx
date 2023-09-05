31f0959564067f746153ce531905d8eca2feb984
Luciano Lee <luciano.lee@webcomm.com.tw>
2023-09-05 14:45:20 +0800

    阻擋連續點擊事件觸發問題

--- nxt_page/src/pages/index.tsx
+++ nxt_page/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 
 interface RickandmortyCharacter {
   id: number,
@@ -32,9 +32,10 @@ interface RickandmortyCharacterRes {
 
 interface PageInfo {
   pageUrl: string;
-  next: string | null,
-  prev: string | null,
+  next: string | null;
+  prev: string | null;
   curr: number;
+  loading: boolean;
 }
 
 export default function Home() {
@@ -44,33 +45,67 @@ export default function Home() {
     pageUrl:'https://rickandmortyapi.com/api/character',
     next: null,
     prev: null,
+    loading: true,
     curr: 0
   });
   const [resData, setResData] = useState<RickandmortyCharacterRes|null>(null)
-  const pageChange = (status: string) => {
-    if (resData) {
-      if (status === 'next') {
-        setPageInfo(pre => ({...pre, pageUrl: pre?.next!, curr: pre.curr + 1 }));
-      }
-      if (status === 'prev') {
-        setPageInfo(pre => ({...pre, pageUrl: pre?.prev!, curr: pre.curr - 1 }));
-      }
+  const chkResData = useMemo(() => resData ,[resData])
+  const pageChange = useCallback((status: string) => {
+    // console.log(pageInfo);
+    setPageInfo(pre => ({...pre, loading: true}));
+    if (status === 'next') {
+      // console.log(pageInfo);
+      fetch(pageInfo.next!)
+      .then((response) => response.json())
+      .then((response: RickandmortyCharacterRes) => {
+        const info = response.info
+        setResData(response)
+        setPageInfo(pre => ({
+          ...pre, 
+          next: info.next, 
+          pageUrl: pageInfo.next!,
+          prev: info.prev, 
+          curr: pre.curr + 1, 
+          loading: false
+        }))
+      });
     }
-  }
+    if (status === 'prev') {
+      fetch(pageInfo.prev!)
+      .then((response) => response.json())
+      .then((response: RickandmortyCharacterRes) => {
+        const info = response.info
+        setResData(response)
+        setPageInfo(pre => ({
+          ...pre, 
+          next: info.next, 
+          pageUrl: pageInfo.prev!,
+          prev: info.prev, 
+          curr: pre.curr - 1, 
+          loading: false
+        }))
+      });
+    }
+  }, [pageInfo])
+  
   useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    fetch(pageInfo.pageUrl, {
-      signal: signal
-    })
-    .then((response) => response.json())
-    .then((response: RickandmortyCharacterRes) => {
-      const info = response.info
-      setResData(response)
-      setPageInfo(pre => ({...pre, next: info.next, prev: info.prev}))
-    });
-    return () => controller.abort();
-  }, [pageInfo.pageUrl, resData])
+    if(!chkResData) {
+      const controller = new AbortController();
+      const signal = controller.signal;
+      setPageInfo(pre => ({...pre, loading: true}));
+      fetch(pageInfo.pageUrl, {
+        signal: signal
+      })
+      .then((response) => response.json())
+      .then((response: RickandmortyCharacterRes) => {
+        const info = response.info
+        // console.log(info);
+        setResData(response)
+        setPageInfo(pre => ({...pre, next: info.next, prev: info.prev, loading: false}))
+      });
+      return () => controller.abort();
+    }
+  }, [pageInfo.pageUrl, chkResData])
   return (
     <main className="container mx-auto">
       <h2>這裡會是首頁主要內容</h2>
@@ -89,12 +124,12 @@ export default function Home() {
         <button 
           className="p-2 mx-2 rounded-lg shadow-md bg-blue-500 hover:bg-blue-300 disabled:bg-gray-100 min-w-40"
           onClick={() => pageChange('prev')}
-          disabled={pageInfo.prev===null} >-</button>
+          disabled={pageInfo.prev===null || pageInfo.loading} >prev</button>
         <div>{pageInfo.curr}</div>
         <button 
           className="p-2 mx-2 rounded-lg shadow-md bg-blue-500 hover:bg-blue-300 min-w-40"
           onClick={() => pageChange('next')}
-          disabled={pageInfo.next===null}>+</button>
+          disabled={pageInfo.next===null || pageInfo.loading}>next</button>
       </div>
       {resData?.results?.map((character) => (
         <div key={character.id}>
